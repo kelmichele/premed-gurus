ffee6a58469a3374ded6cb437edc237c6d9a24a9
Kelli Parris <kelli@hsdesignhouse.com>
2018-10-16 09:27:28 -0500

    removed tmp home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,18 @@
 class StaticPagesController < ApplicationController
+	def home
+	end
+
+	def advisers
+	end
+
+	def services
+		@full_title = "Complete Service Package"
+		@assessment_title = "Advanced Assessment of Credentials"
+		@mcat_title = "MCAT Strategies"
+		@amcas_title = "AMCAS Application"
+		@essays_title = "Secondary Essays"
+		@interviews_title = "Interview Preparation"
+		@school_title = "Final Selection of Medical School"
+		@skills_title = "Learning Skills and Strategies"
+	end
 end
